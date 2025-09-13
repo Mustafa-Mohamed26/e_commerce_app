df7a1293cc75c109e84fc3fd9044e2a3468c938f
Mustafa-Mohamed26 <mustafamohameda447@gmail.com>
2025-09-13 17:34:58 +0300

    + adding Authentication

---
+++ lib/api/models/response/auth_response.dart
@@ -0,0 +1,53 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'auth_response.g.dart';
+
+@JsonSerializable()
+class AuthResponse {
+  @JsonKey(name: "message")
+  final String? message;
+  @JsonKey(name: "user")
+  final User? user;
+  @JsonKey(name: "token")
+  final String? token;
+
+  AuthResponse ({
+    this.message,
+    this.user,
+    this.token,
+  });
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return _$AuthResponseFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() {
+    return _$AuthResponseToJson(this);
+  }
+}
+
+@JsonSerializable()
+class User {
+  @JsonKey(name: "name")
+  final String? name;
+  @JsonKey(name: "email")
+  final String? email;
+  @JsonKey(name: "role")
+  final String? role;
+
+  User ({
+    this.name,
+    this.email,
+    this.role,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return _$UserFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() {
+    return _$UserToJson(this);
+  }
+}
+
+
